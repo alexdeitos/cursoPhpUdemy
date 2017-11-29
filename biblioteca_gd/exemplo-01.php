b642b2614743399bbc20cf6b506d8e9ec5dd5a1d
alexdeitos <alexdeitos01@gmail.com>
2017-11-29 21:44:37 -0200

    alteracao no arquivo biblioteca gd

--- biblioteca_gd/exemplo-01.php
+++ biblioteca_gd/exemplo-01.php
@@ -3,6 +3,14 @@
 	header("Content-Type: image/png");
 
 	$image = imagecreate(256,256);
-	
+
+	$black = imagecolorallocate($image, 0, 0, 0); //primeiro parametro é a variavel, depois padrão rgb e seus valores
+	$red = imagecolorallocate($image, 255, 0, 0);
+
+	imagestring($image,5,60,120,"Curso PHP 7",$red);
+
+	imagepng($image);
+
+	imagedestroy($image);
 
 ?>
