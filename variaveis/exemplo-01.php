047c872fa7c15a0675688e19f6390a375176bd70
Alexsandro Deitos <alexdeitos01@gmail.com>
2018-03-19 15:30:58 -0300

    adição da pasta variaveis

---
+++ variaveis/exemplo-01.php
@@ -0,0 +1,13 @@
+<?php
+
+$sql = addslashes($_POST['sql']);
+
+echo "SQL digitado na pagina1: <br>".$sql;
+
+?>
+
+<html>
+<body>
+    <p><a href="http://localhost/cursoPhpUdemy/variaveis/index.html">volta para a página inicial</a></p>
+</body>
+</html>
